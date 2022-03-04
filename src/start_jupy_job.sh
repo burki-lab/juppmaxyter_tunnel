6bc7930f58bd287cf3bc9d4577456ef4eb135323
biothomme <55461592+biothomme@users.noreply.github.com>
2022-03-04 12:14:57 +0100

    update to use config file

---
+++ src/start_jupy_job.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# (A) as a first step we load the yaml reader and read the
+# config file (~/config.yml) into $config_<...>
+. ../yaml_reader/parse_yaml.sh
+eval `parse_yaml ../config.yml "config_"`
+
+# (B) we load the input variables and commands
+# jobname is always jupyter to enable simple cancel
+JOBNAME="jupyter"
+
+# cores, project and server are found in the config file
+CORE_TYPE=$config_server_coretype
+NUM_CORES=$config_server_numcores
+PROJECT=$config_server_project
+SERVER=$config_server_name
+
+# jobinfo
+DURATION=$config_server_duration
+LOGFILE="$config_server_outputdir/@A.log"
+
+# mail info
+MAIL_ADD=$config_mail_address
+MAIL_TYPE=$config_mail_type
+
+# (C) building the main command 
+# jupyter connection settings
+IP=$config_connection_ip
+PORT=$config_connection_port
+
+# assemble jupy command
+CMD="jupyter lab --no-browser --ip=$IP --port=$PORT"
+
+
+# (D) finally submit the job
+
+echo "sbatch -J $JOBNAME -A $PROJECT -M $SERVER -p $CORE_TYPE -n $NUM_CORES -t $DURATION -o $LOGFILE --mail-user $MAIL_ADD --mail-type $MAIL_TYPE $CMD"
